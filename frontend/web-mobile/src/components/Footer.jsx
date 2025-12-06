959397e6dc5c2139b00fb3ca9185d2953833e893
Wiktoria Głuszko <wiktoria.gluszko2016@gmail.com>
2025-12-06 12:25:27 +0100

    empty template

---
+++ frontend/web-mobile/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Hackathon App</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ frontend/web-mobile/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h2>Nazwa Projektu</h2>
+      <nav>
+        <a href="#about">O nas</a>
+        <a href="#features">Funkcje</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
